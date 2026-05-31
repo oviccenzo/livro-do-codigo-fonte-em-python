6dda20ed76f943aa40e89e9e22c493f65493589b
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2026-05-31 08:47:58 -0300

    Add files via upload

---
+++ pense-em-python/pense-em-python/Capitulo 1: jornada do programa/1.5-valores e tipos.py
@@ -0,0 +1,54 @@
+##1.5 - valores e tipos
+
+print(type(2))
+
+print(type(42.0))
+
+print(type('hello world!'))
+
+print(type('2'))
+
+print(type('42.0'))
+
+print(type('hello world!'))
+
+print(type(True))
+
+print(type(False))
+
+print(type(229))
+
+print(type(32*2))
+
+print(1,000,000,000)
+
+print(2,000,000,000)
+
+print(222*2,333*3,444*4,555*5,666*6,777*7,888*9,999*10)
+
+# Resultado do exemplo 1.5:
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+# <class 'str'>
+# <class 'str'>
+# <class 'str'>
+# <class 'bool'>
+# <class 'bool'>
+# <class 'int'>
+# <class 'int'>
+# 1 0 0 0
+# 2 0 0 0
+# 444 999 1776 2775 3996 5439 7992 9990
+
+###exercicio do capitulo 1
+
+# print('olá)
+
+# print(olá)
+
+# 2++2
+
+# 02
+
+# 5 5
