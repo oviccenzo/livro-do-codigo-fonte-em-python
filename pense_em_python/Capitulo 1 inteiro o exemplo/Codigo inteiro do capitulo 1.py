8ae72bf334d2c1eb4bcec8c7dcd4747d323f8912
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2026-06-03 15:41:26 -0300

    Add files via upload

---
+++ pense_em_python/Capitulo 1 inteiro o exemplo/Codigo inteiro do capitulo 1.py
@@ -0,0 +1,64 @@
+#Capitulo 1: jornada do programa
+
+##1.3 -primeiro programa
+
+print("hello world")
+
+print('hello world!')
+
+print('meu nome é viccenzo de oliveira')
+print('eu estou cursando o superior chamado ciência da computação')
+print('eu sou portador de deficiência audtitiva')
+print('eu tenho 23 ano e vou fazer 24 em junho dia 27 desse ano')
+
+##1.4 operadores aritmeticos
+
+print(40 + 2)
+
+print(43-2)
+
+print(6*7)
+
+print((6**2) + 6)
+
+print(6 ^ 2)
+
+##1.5 - valores e tipos
+
+print(type(2))
+
+print(type(42.0))
+
+print(type('hello world!'))
+
+print(type('2'))
+
+print(type('42.0'))
+
+print(type('hello world!'))
+
+print(type(True))
+
+print(type(False))
+
+print(type(229))
+
+print(type(32*2))
+
+print(1,000,000,000)
+
+print(2,000,000,000)
+
+print(222*2,333*3,444*4,555*5,666*6,777*7,888*9,999*10)
+
+###exercicio do capitulo 1
+
+# print('olá)
+
+# print(olá)
+
+# 2++2
+
+# 02
+
+# 5 5
