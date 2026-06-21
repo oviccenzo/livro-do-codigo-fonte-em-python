2e410a73a27046f3973d1fa896c16e940947f4e8
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2026-06-20 22:11:02 -0300

    Delete pense_em_python directory

--- pense_em_python/Capitulo 3 - funcoes/Exemplo-3.2/Composicao/composicao - exemplo da funcao def o print_lyric1.py
+++
@@ -1,9 +0,0 @@
-def print_lyric():
-  print("I'm a lumberjack, and I'm okay.")
-  print("I sleep all night and I work all day.")
-
-print_lyric()
-
-# O resultado da funcao print_lyric eh:
-# I'm a lumberjack, and I'm okay.
-# I sleep all night and I work all day.
