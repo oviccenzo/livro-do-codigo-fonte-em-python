8b9d4124febbba63726c64ed6cff82a9b276e880
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2026-06-21 10:34:44 -0300

    Add files via upload

---
+++ pense_em_python/Exercicio_do_livro_pense_em_python/exercicio_do_capitulo_9.py
@@ -0,0 +1,104 @@
+# ExercíciosHá soluções para estes exercícios na próxima seção. Mas é bom você tentar
+# fazer cada um antes de ver as soluções.
+# Exercício 9.1
+# Escreva um programa que leia words.txt e imprima apenas as palavras com
+# mais de 20 caracteres (sem contar whitespace).
+
+# Exercício 9.2
+# Em 1939, Ernest Vincent Wright publicou uma novela de 50.000 palavras,
+# chamada Gadsby, que não contém a letra “e”. Como o “e” é a letra mais
+# comum em inglês, isso não é algo fácil de fazer.
+# Na verdade, é difícil até construir um único pensamento sem usar o símbolo
+# mais comum do idioma. No início é lento, mas com prudência e horas de
+# treino, vai ficando cada vez mais fácil.
+# Muito bem, agora eu vou parar.
+# Escreva uma função chamada has_no_e que retorne True se a palavra dada
+# não tiver a letra “e” nela.
+# Altere seu programa na seção anterior para imprimir apenas as palavras que
+# não têm “e” e calcule a porcentagem de palavras na lista que não têm “e”.
+
+# Exercício 9.3
+# Escreva uma função chamada avoids que receba uma palavra e uma série de
+# letras proibidas, e retorne True se a palavra não usar nenhuma das letras
+# proibidas.
+# Altere o código para que o usuário digite uma série de letras proibidas e o
+# programa imprima o número de palavras que não contêm nenhuma delas.
+# Você pode encontrar uma combinação de cinco letras proibidas que exclua o
+# menor número possível de palavras?
+
+# Exercício 9.4
+# Escreva uma função chamada uses_only que receba uma palavra e uma série
+# de letras e retorne True, se a palavra só contiver letras da lista. Você pode
+# fazer uma frase usando só as letras acefhlo? Que não seja “Hoe alfalfa?”
+
+# Exercício 9.5
+# Escreva uma função chamada uses_all que receba uma palavra e uma série de
+# letras obrigatórias e retorne True se a palavra usar todas as letras obrigatórias
+# pelo menos uma vez. Quantas palavras usam todas as vogais (aeiou)? E que
+# tal aeiouy?
+
+# Exercício 9.6
+# Escreva uma função chamada is_abecedarian que retorne True se as letras
+# numa palavra aparecerem em ordem alfabética (tudo bem se houver letras
+# duplas). Quantas palavras em ordem alfabética existem?
+# Busca
+# Todos os exercícios na seção anterior têm algo em comum; eles podem ser
+# resolvidos com o modelo de busca que vimos em “Buscando”, na página 123.
+# O exemplo mais simples é:
+# def has_no_e(word):
+# for letter in word:
+# if letter == ‘e’:
+# return False
+# return True
+
+# Exercícios
+# Exercício 9.7
+# Esta pergunta é baseada em um quebra-cabeça veiculado em um programa de
+# rádio chamado Car Talk (http://www.cartalk.com/content/puzzlers):
+# Dê uma palavra com três letras duplas consecutivas. Vou dar exemplos de
+# palavras que quase cumprem a condição, mas não chegam lá. Por exemplo, a
+# palavra committee, c-o-m-m-i-t-t-e-e. Seria perfeita se não fosse aquele ‘i’
+# que se meteu ali no meio. Ou Mississippi: M-i-s-s-i-s-s-i-p-p-i. Se pudesse
+# tirar aqueles ‘is’, daria certo. Mas há uma palavra que tem três pares
+# consecutivos de letras e, que eu saiba, pode ser a única palavra que existe. É
+# claro que provavelmente haja mais umas 500, mas só consigo pensar nessa.
+# Qual é a palavra?
+# Escreva um programa que a encontre.
+# Solução: http://thinkpython2.com/code/cartalk1.py.
+
+# Exercício 9.8
+# Aqui está outro quebra-cabeça do programa Car Talk
+# (http://www.cartalk.com/content/puzzlers):
+# “Estava dirigindo outro dia e percebi algo no hodômetro que chamou a minha
+# atenção. Como a maior parte dos hodômetros, ele mostra seis dígitos, apena
+# sem milhas inteiras. Por exemplo, se o meu carro tivesse 300.000 milhas, eu
+# veria 3-0-0-0-0-0.
+# “Agora, o que vi naquele dia foi muito interessante. Notei que os últimos 4
+# dígitos eram um palíndromo; isto é, podiam ser lidos da mesma forma no
+# sentido correto e no sentido inverso. Por exemplo, 5-4-4-5 é um palíndromo,
+# então no meu hodômetro poderia ser 3-1-5-4-4-5.
+# “Uma milha depois, os últimos 5 números formaram um palíndromo. Por
+# exemplo, poderia ser 3-6-5-4-5-6. Uma milha depois disso, os 4 números do
+# meio, dentro dos 6, formavam um palíndromo. E adivinhe só? Um milha
+# depois, todos os 6 formavam um palíndromo!
+# “A pergunta é: o que estava no hodômetro quando olhei primeiro?”
+# Escreva um programa Python que teste todos os números de seis dígitos e
+# imprima qualquer número que satisfaça essas condições.
+# Solução: http://thinkpython2.com/code/cartalk2.py.
+
+# Exercício 9.9
+# Aqui está outro problema do Car Talk que você pode resolver com uma busca
+# (http://www.cartalk.com/content/puzzlers):
+# “Há pouco tempo recebi uma visita da minha mãe e percebemos que os dois
+# dígitos que compõem a minha idade, quando invertidos, representavam a
+# idade dela. Por exemplo, se ela tem 73 anos, eu tenho 37 anos. Ficamos
+# imaginando com que frequência isto aconteceu nos anos anteriores, mas
+# acabamos mudando de assunto e não chegamos a uma resposta.
+# “Quando cheguei em casa, cheguei à conclusão de que os dígitos das nossas
+# idades tinham sido reversíveis seis vezes até então. Também percebi que, se
+# tivéssemos sorte, isso aconteceria novamente dali a alguns anos, e se
+# fôssemos muito sortudos, aconteceria mais uma vez depois disso. Em outras
+# palavras, aconteceria 8 vezes no total. Então a pergunta é: quantos anos tenho
+# agora?”Escreva um programa em Python que busque soluções para esse problema.
+# Dica: pode ser uma boa ideia usar o método de string zfill.
+# Solução: http://thinkpython2.com/code/cartalk3.py.
