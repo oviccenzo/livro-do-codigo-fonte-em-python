5cdd89943e105196c84648d492d164a62bde4d9b
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2026-07-01 15:00:36 -0300

    Delete Capitulo 6 - funcoes com resultados.py directory

--- Capitulo 6 - funcoes com resultados.py/Valores_de_retorno/O_valor_retorna_none.py
+++
@@ -1,7 +0,0 @@
-def absolute_value1(x7):
-    if x7 < 0:
-        return -x7
-    if x7 > 0:
-        return x7
-
-print(absolute_value1(0))
--- Capitulo 6 - funcoes com resultados.py/Valores_de_retorno/valor_absoluto.py
+++
@@ -1,7 +0,0 @@
-def absolute_value(x6):
-    if x6 < 0:
-        return -x6
-    else:
-        return x6
-x6 = float(input("Digite qualquer número: "))
-print(f"O valor absolute eh: {absolute_value(x6)}")
