90302e4346cffe9453245f5466c3bcccc938dbda
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2026-07-24 22:38:48 -0300

    Add files via upload

---
+++ pense_em_python/Capitulo 4 - estudo de caso - projeto interface/4.4 - encapsulamento/exemplo1.py
@@ -0,0 +1,14 @@
+import turtle
+
+bob = turtle.Turtle()
+print(bob)
+
+def square(t):
+    for i in range(4):
+        t.fd(100)
+        t.lt(90)
+square(bob)
+
+alice = turtle.Turtle()
+square(alice)
+turtle.mainloop()
---
+++ pense_em_python/Capitulo 4 - estudo de caso - projeto interface/4.7 - refatoracao/exemplo3.py
@@ -0,0 +1,44 @@
+import turtle
+import math
+
+bob = turtle.Turtle()
+print(bob)
+
+def arc(t,r,angle):
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = angle / n
+    for i in range(n):
+        t.fd(step_length)
+        t.lt(step_angle)
+
+def polyline(t, n, length, angle):
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+def polygon(t, n, length, angle):
+    angle = 360.0 / n
+    polyline(t, n, length, angle)
+
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+    polygon(t, n, step_length, step_angle)
+
+def circle(t, r):
+    arc(t, r, 360)
+
+
+
+
+polyline(bob,200,3,3)
+
+polygon(bob,23,45,34)
+
+circle(bob, 100)
+
+turtle.mainloop()
