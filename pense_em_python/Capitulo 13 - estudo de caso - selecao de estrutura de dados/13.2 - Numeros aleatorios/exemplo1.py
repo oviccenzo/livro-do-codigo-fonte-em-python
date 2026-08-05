5d46abb03dba5a1e3472bd5469a57cd8c810439d
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2026-08-05 11:16:59 -0300

    Add files via upload

---
+++ pense_em_python/Capitulo 13 - estudo de caso - selecao de estrutura de dados/13.2 - Numeros aleatorios/exemplo1.py
@@ -0,0 +1,6 @@
+import random
+
+for i in range(10):
+    x = random.random(5, 10)
+    print(x)
+    print("\n")
---
+++ pense_em_python/Capitulo 13 - estudo de caso - selecao de estrutura de dados/13.2 - Numeros aleatorios/exemplo2.py
@@ -0,0 +1,7 @@
+import random
+
+t = [1,2,3,4,10]
+sorteio = random.choice(t)
+print(sorteio)
+
+print("\n")
